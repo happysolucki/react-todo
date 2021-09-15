f7f7a3ac58e11d79d8e89c0988c20c6835661378
Brandon Keefer <happysolucki@outlook.com>
2021-09-15 11:04:13 -0500

    removed unused code

--- src/components/App.js
+++ src/components/App.js
@@ -108,19 +108,4 @@ function App() {
   );
 }
 
-// const sampleTasks = [
-//   {
-//     id: 1,
-//     text: "Doctors Appointment",
-//     day: "Feb 5th at 2:30pm",
-//     reminder: true,
-//   },
-//   {
-//     id: 2,
-//     text: "Meeting at School",
-//     day: "Feb 6th at 1:30pm",
-//     reminder: true,
-//   },
-// ];
-
 export default App;
