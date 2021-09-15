d8326ee7a0233012bb4641d3d6652a5da5088025
Brandon Keefer <happysolucki@outlook.com>
2021-09-15 04:05:42 -0500

    finished tutorial

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+export const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
